7c367688dd5dafbb4d5ff4832a9a03c822a54549
Alex Rasmussen <arasmussen64@gmail.com>
2022-01-17 19:29:13 -0700

    easy stuff

--- IntroPythonChatbots.py
+++ IntroPythonChatbots.py
@@ -1,2 +1,35 @@
 my_name = "Alex Rasmussen"
 print("Hello and welcom " + my_name + "!")
+
+#This is a comment
+#Below is how to print - Don't forget the quotes!
+print("Hello world!")
+
+print("Can use single or double quotes, doesn't matter!")
+
+#variables
+my_variable = "this variable has a string"
+num_variable = 4
+
+#Types of errors
+err1 = "SyntaxError"
+print("You messed up the code")
+err2 = "NameError"
+print("Python interpreter sees a word it doesn't recognize like a variable that as never defined")
+print("Python puts an ^ where the error occurs")
+
+#integers and floats
+my_integer = 2
+my_floatingPointNumber = 4.0
+
+print("Python converts all ints to floats before doing division")
+print("If you try to divide by zero you'll get a ZeroDivisionError")
+print("Python follows the order of operations")
+
+
+add_subtract = 573 - 74 + 1
+mult = 25 * 2
+div = 10 / 5
+
+#Floating Point Arithmetic Issues and Limitations
+#https://docs.python.org/3/tutorial/floatingpoint.html
